4334769109bb030d2db7d8e49b5de2fe7c7d8e36
VishnuKailash <vishnukailash.bookshelf@gmail.com>
2024-10-08 12:23:07 +0530

    add

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -11,7 +11,7 @@ class ApiService {
   Future<Response> fetchDataFromApi() async {
     final url = Uri.parse("https://66fbb69f8583ac93b40cdf37.mockapi.io/sample");
     var response = await http.get(url);
-    print("the respone is:$response");
+    print("the respone is:${response.body}");
     return response;
 
   }
